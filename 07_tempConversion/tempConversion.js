4b32244c37d7880a852d35a90c126f6ef54fcc04
Aaron P Rosenfeld <ashheels@gmail.com>
2023-06-29 14:21:34 -0500

    Leap years, temp conversion passing

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,9 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(degreesFarenheit) {
+  return parseFloat(((degreesFarenheit - 32) * 5 / 9).toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(degreesCelsius) {
+  return parseFloat((degreesCelsius * 9 / 5 + 32).toFixed(1));
 };
 
 // Do not edit below this line
